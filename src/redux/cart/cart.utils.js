0333e5578f3d3695323e9d9b79c4134ac4c33a26
Aaron <aaronmassey45@gmail.com>
2019-07-09 06:58:58 -0500

    Fixed error causing nullified cart when trying to decrease item amount

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -23,7 +23,7 @@ export const removeItemFromCart = (cartItems, cartItemToRemove) => {
     return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
   }
 
-  cartItems.map(cartItem =>
+  return cartItems.map(cartItem =>
     cartItem.id === cartItemToRemove.id
       ? { ...cartItem, quantity: cartItem.quantity - 1 }
       : cartItem
