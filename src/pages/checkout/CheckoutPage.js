d02da0bd144477004d29f8bef997d682ac3a5e91
Aaron <aaronmassey45@gmail.com>
2019-07-09 06:48:10 -0500

    Fixed spelling error

--- src/pages/checkout/CheckoutPage.js
+++ src/pages/checkout/CheckoutPage.js
@@ -39,7 +39,7 @@ const CheckoutPage = ({ cartItems, total }) => {
         <span>TOTAL: ${total}</span>
       </div>
       <div className="test-warning">
-        *Please use the following test credit care for payments*
+        *Please use the following test credit card for payments*
         <br />
         4242 4242 4242 4242 - Exp: 01/24- CVV: 123
       </div>
